eae56a3ec518370cbc37a98746bcde669d25bd13
Erwan Bouriau <bouriaue@iut1i-702-a15.local>
2019-12-03 11:31:52 +0100

    collection view

--- PlacesWhereYouCantGO/PlacesWhereYouCantGO/ViewController.swift
+++ PlacesWhereYouCantGO/PlacesWhereYouCantGO/ViewController.swift
@@ -8,13 +8,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    @IBOutlet var collectionView: UICollectionView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        collectionView.register(UINib(nibName: "PlacesCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "placesCollectionViewCell")
+        
     }
-
-
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+           return 1
+       }
+       
+       func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "placesCollectionViewCell", for: indexPath)
+            as! PlacesCollectionViewCell
+        cell.displayContent(title: "TEST")
+        return cell
+       }
 }
 
