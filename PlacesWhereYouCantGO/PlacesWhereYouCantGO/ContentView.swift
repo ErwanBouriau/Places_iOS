d2b93e79d24f233ec5986b0feb63362381804303
Erwan Bouriau <bouriaue@iut1i-702-a15.local>
2019-12-03 08:15:24 +0100

    start

---
+++ PlacesWhereYouCantGO/PlacesWhereYouCantGO/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PlacesWhereYouCantGO
+//
+//  Created by Erwan Bouriau on 03/12/2019.
+//  Copyright © 2019 Erwan Bouriau. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
